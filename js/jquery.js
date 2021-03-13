d5afff9fe7e74ebc1f1cf22b6f601f70b41fc26f
EC2 Default User <ec2-user@ip-172-31-3-104.ap-northeast-1.compute.internal>
2021-03-13 02:58:34 +0000

    second commit

--- js/jquery.js
+++ js/jquery.js
@@ -2,6 +2,17 @@ $(function(){
   $('#single-item').slick({
       autoplay:true, // 自動再生を設定
       autoplaySpeed:3000, // スライド切り替えの時間を設定
-      dots:true // インジケーターを表示
+      dots:true, // インジケーターを表示
     });
+  window.onscroll = function(){
+    var scrollTop = window.pageYOffset ;   //スクロール量を取得
+    if (scrollTop > 200 ) { 　　
+      $('#main-header').css('background-color', 'black');
+      $('#main-header').css('transition', '1.3s')
+    }
+    if (scrollTop < 200 ) { 
+      $('#main-header').css('background-color', '');
+      $('#main-header').css('transition', '1.0s')
+    }
+  }
 });
